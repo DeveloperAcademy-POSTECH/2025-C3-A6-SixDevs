17b8bf31282d6c5121ddad21ba9264a6e4788136
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-02 17:13:57 +0900

    chore: 프로젝트 폴더링 수정(Modules -> Presentations)

--- PodongPodong/PodongPodong/Presentations/PartyList/Views/PartyListView.swift
+++ PodongPodong/PodongPodong/Presentations/PartyList/Views/PartyListView.swift
@@ -1,5 +1,5 @@
 //
-//  HomeView.swift
+//  PartyListView.swift
 //  PodongPodong
 //
 //  Created by 김진혁 on 6/1/25.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct HomeView: View {
+struct PartyListView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    HomeView()
+    PartyListView()
 }
