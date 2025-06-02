a1d5e2cdb7426b53a872f9ae3ddad63d1b149b82
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-02 15:55:46 +0900

    feat: user 구현

--- PodongPodong/PodongPodong/Models/Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  model.swift
-//  PodongPodong
-//
-//  Created by 김진혁 on 6/1/25.
-//
-
-import Foundation
---
+++ PodongPodong/PodongPodong/Models/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  PodongPodong
+//
+//  Created by 김진혁 on 6/1/25.
+//
+
+struct User {
+    let email: String // UUID
+    let nickName: String // 앱 활동 이름
+    let profileImage: ProfileImage    // Asset 에서 불러오기
+    let reviewRating: Double
+
+    // TODO: Asset 이름과 동일하게 수정하기
+    enum ProfileImage {
+        case image1
+        case image2
+        case image3
+        case image4
+        
+        var profileImage: String {
+            switch self {
+            case .image1:
+                return "1번 이미지" // FIXME: -
+            case .image2:
+                return "2번 이미지" // FIXME: -
+            case .image3:
+                return "3번 이미지" // FIXME: -
+            case .image4:
+                return "4번 이미지" // FIXME: -
+            }
+        }
+    }
+}
