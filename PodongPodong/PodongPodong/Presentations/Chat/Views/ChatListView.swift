8caca0430e2b760c0cf2d45edb3bff3f02114b7e
Velko <wlsgurrla716@naver.com>
2025-06-03 23:43:25 +0900

    feat:#8 ChatList 구현

--- PodongPodong/PodongPodong/Presentations/Chat/Views/ChatListView.swift
+++ PodongPodong/PodongPodong/Presentations/Chat/Views/ChatListView.swift
@@ -7,12 +7,34 @@
 
 import SwiftUI
 import SendbirdSwiftUI
+import SendbirdChatSDK
 
 struct ChatListView: View {
-    @StateObject var provider = GroupChannelListViewProvider()
+    private var chatListVM = ChatListViewModel(userId: "rlawlsgur716")
     
     var body: some View {
-        GroupChannelListView(provider: provider)
+        GroupChannelListView(provider: chatListVM.getProvider()) // 데이터 트리거 역활만
+            .frame(width: 0, height: 0)
+            .hidden()
+        
+        List {
+            ForEach(chatListVM.filteredChannels, id: \.channelURL) { channel in
+                if let lastMessage = channel.lastMessage as? UserMessage {
+                    let memberCount = channel.memberCount
+                    let message = lastMessage.message
+                    let date = Date(timeIntervalSince1970: TimeInterval(lastMessage.createdAt) / 1000)
+                    let unreadMessageCount = Int(channel.unreadMessageCount)
+                    ChatListItem(
+                        title: channel.name,
+                        memberCount: Int(memberCount),
+                        profileImage: "person.circle.fill",
+                        lastMessage: message,
+                        lastMessageDate: date,
+                        unreadMessageCount: unreadMessageCount
+                    )
+                }
+            }
+        }
     }
 }
 
