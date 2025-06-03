de9c8b4b6d5323e9edfe8863572a150141e2dbfc
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-04 01:15:56 +0900

    feat: 온보딩 화면 구현

---
+++ PodongPodong/PodongPodong/Presentations/Login/Views/OnboardingView.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingView.swift
+//  PodongPodong
+//
+//  Created by 김민석 on 6/4/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        ZStack {
+            Color.primaryLight1
+                .ignoresSafeArea()
+            NavigationStack {
+                VStack(spacing: 150) {
+                    // TODO: - 앱 로고 넣기
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    NavigationLink(destination: LoginView()) {
+                        ActionButtonView(title: "포스텍 이메일 인증하기", isEnabled: true)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
