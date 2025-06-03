01c746349f8683031b12ed1388c3c73b199e0c3b
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-04 01:42:54 +0900

    refactor: 온보딩 화면 코드 정리

--- PodongPodong/PodongPodong/Presentations/Login/Views/OnboardingView.swift
+++ PodongPodong/PodongPodong/Presentations/Login/Views/OnboardingView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        ZStack {
-            Color.primaryLight1
-                .ignoresSafeArea()
-            NavigationStack {
-                VStack(spacing: 150) {
-                    // TODO: - 앱 로고 넣기
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    NavigationLink(destination: EmailInputView()) {
-                        ActionButtonView(title: "포스텍 이메일 인증하기", isEnabled: true)
-                    }
+        NavigationStack {
+            VStack(spacing: 150) {
+                // TODO: - 앱 로고 넣기
+                Image(systemName: "globe")
+                
+                NavigationLink {
+                    EmailInputView()
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    ActionButtonView(title: "포스텍 이메일 인증하기", isEnabled: true)
+                        .frame(maxWidth: .infinity)
                 }
+                .padding(.horizontal, 24)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.primaryLight1.ignoresSafeArea())
         }
     }
 }
