e8636f59c5bb9614bdf80ec94d174ad9f91cd51b
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-03 18:50:53 +0900

    feat: 큰 액션 버튼 구현

---
+++ PodongPodong/PodongPodong/Components/ActionButtonView.swift
@@ -0,0 +1,31 @@
+//
+//  ButtonComponent.swift
+//  PodongPodong
+//
+//  Created by 김민석 on 6/3/25.
+//
+
+import SwiftUI
+
+struct ActionButtonView: View {
+    var title: String
+    var isEnabled: Bool = true
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.pretendardSemibold18)
+                .foregroundColor(isEnabled ? .secondary : .gray30)
+                .frame(width: 361, height: 56)
+                .background(isEnabled ? Color.primaryColor : Color.gray10)
+                .cornerRadius(12)
+        }
+        .disabled(!isEnabled)
+    }
+}
+
+#Preview {
+    ActionButtonView(title: "실행하기", action: {})
+    ActionButtonView(title: "실행하기", isEnabled: false, action: {})
+}
