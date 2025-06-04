cf2d86b6024cab302f18b43b969312fe3d3caa47
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-04 21:47:50 +0900

    feat: 상단 바 부분 구현

--- PodongPodong/PodongPodong/Presentations/PartyList/ViewModels/PartyListViewModel.swift
+++ PodongPodong/PodongPodong/Presentations/PartyList/ViewModels/PartyListViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+final class PartyListViewModel {
+    private(set) var partyList: [Party] = []
+    
+    init() {
+        let sampleUser = User(
+            email: "alstjr7437@postech.ac.kr",
+            nickName: "심성이착한사람",
+            profileImage: .image1,      // User.ProfileImage 중 하나
+            reviewRating: 4.7
+        )
+        
+        let dummyParty = Party(
+                   writen: sampleUser,
+                   title: "부산에서 같이 장보기 해요",
+                   category: .meat,
+                   orderType: .groupPurchase,
+                   recruitmentCount: 3,
+                   purchaseChannel: .online,
+                   purchaseLocation: "https://shopping.example.com/store/5678",
+                   totalPrice: 30000,
+                   weightAndCount: .weight,
+                   amount: 2,
+                   appointment: Party.Appointment(
+                       date: Date().addingTimeInterval(3600 * 24),
+                       time: Date().addingTimeInterval(3600 * 25),
+                       location: "부산 해운대 시장"
+                   ),
+                   description: "함께 주문하면 배송비 아깝지 않아요! 같이 가실 분 구합니다.",
+                   chatURL: "https://chat.example.com/room/XYZ789"
+               )
+        
+        partyList.append(dummyParty)
+    }
+}
--- PodongPodong/PodongPodong/Presentations/PartyList/Views/PartyListView.swift
+++ PodongPodong/PodongPodong/Presentations/PartyList/Views/PartyListView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct PartyListView: View {
+    @State private var viewModel = LoginViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack {
     }
 }
 
