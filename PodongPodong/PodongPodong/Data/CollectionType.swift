6db3b799c130f6e06149fc7323bebe3ced299264
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-04 20:12:21 +0900

    docs: protocol 설명 주석 추가

--- PodongPodong/PodongPodong/Data/CollectionType.swift
+++ PodongPodong/PodongPodong/Data/CollectionType.swift
@@ -5,6 +5,9 @@
 //  Created by 김민석 on 6/4/25.
 //
 
+/// Firebase에 데이터를 저장하기 위한 key 값
+/// 해당 부분으로 데이터 식별
+/// User -> User 부분으로 가서 데이터를 검색
 enum CollectionType: String {
     case user = "User"
     case party = "Party"
