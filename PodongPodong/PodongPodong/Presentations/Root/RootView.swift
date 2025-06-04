23f7312b95e357424b3747aeaef89773232074aa
Velko <wlsgurrla716@naver.com>
2025-06-05 01:38:53 +0900

    feat:#20 리스트 화면이동 구현

--- PodongPodong/PodongPodong/Presentations/Root/RootView.swift
+++ PodongPodong/PodongPodong/Presentations/Root/RootView.swift
@@ -6,29 +6,42 @@
 //
 
 import SwiftUI
+import SendbirdSwiftUI
 
 struct RootView: View {
+    @State var router: NavigationRouter = .init()
+    
     var body: some View {
-        TabView {
-            
-            Tab("파티 목록", systemImage: "text.page.badge.magnifyingglass") {
-                PartyListView()
+        NavigationStack(path: $router.destination) {
+            TabView {
+                Tab("파티 목록", systemImage: "text.page.badge.magnifyingglass") {
+                    PartyListView()
+                }
+                
+                Tab("내 파티", systemImage: "person.2") {
+                    MyPartiesView()
+                }
+                
+                Tab("채팅", systemImage: "message") {
+                    ChatListView()
+                }
+                
+                Tab("마이", systemImage: "person") {
+                    ProfileView()
+                }
             }
-            
-            Tab("내 파티", systemImage: "person.2") {
-                MyPartiesView()
-            }
-            
-            Tab("채팅", systemImage: "message") {
-                ChatListView()
-            }
-            
-            Tab("마이", systemImage: "person") {
-                ProfileView()
+            .environmentObject(router)
+            .navigationDestination(for: NavigationDestination.self) { route in
+                switch route {
+                case .chatView(let channelURL):
+                    ChatView(provider: GroupChannelViewProvider(channelURL: channelURL))
+                }
             }
         }
+        
     }
 }
+
 
 #Preview {
     RootView()
