51d5bbeec17b78d6d406d18315efe4e3a85e981a
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-04 20:09:51 +0900

    feat: Encodable extension 구현

---
+++ PodongPodong/PodongPodong/Extension/EncodableExtension.swift
@@ -0,0 +1,20 @@
+//
+//  EncodableExtension.swift
+//  PodongPodong
+//
+//  Created by 김민석 on 6/4/25.
+//
+
+import Foundation
+
+extension Encodable {
+    var asDictionary: [String: Any]? {
+        guard let object = try? JSONEncoder().encode(self),
+              let dictionary = try? JSONSerialization.jsonObject(with: object, options: [])
+                as? [String: Any] else {
+            return nil
+        }
+        
+        return dictionary
+    }
+}
