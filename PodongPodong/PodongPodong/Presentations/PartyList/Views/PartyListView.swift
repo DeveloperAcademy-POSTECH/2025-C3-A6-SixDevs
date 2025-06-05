ac2cebe3d50eedf58a052e258930082dc8dc9337
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-05 13:11:56 +0900

    feat: PartyList 네비게이션바 구현

--- PodongPodong/PodongPodong/Presentations/PartyList/Views/PartyListView.swift
+++ PodongPodong/PodongPodong/Presentations/PartyList/Views/PartyListView.swift
@@ -13,6 +13,44 @@ struct PartyListView: View {
         NavigationView {
             ScrollView {
                 VStack {
+            .navigationBarItems(
+                leading: PartyListNavBarTitle(),
+                trailing: PartyListNavBarButtons(
+                    searchButtonAction: {},
+                    bellButtonAction: {}
+                )
+    }
+}
+
+// MARK: - navigationBar View Items
+
+extension PartyListView {
+    struct PartyListNavBarTitle: View {
+        var body: some View {
+            Text("포동포동")
+                .font(.pretendardSemibold24)
+                .foregroundColor(.primaryColor)
+        }
+    }
+
+    struct PartyListNavBarButtons: View {
+        let searchButtonAction: () -> Void
+        let bellButtonAction: () -> Void
+
+        var body: some View {
+            HStack(spacing: 16) {
+                Button(action: searchButtonAction) {
+                    Image(systemName: "magnifyingglass")
+                        .font(.system(size: 24))
+                        .foregroundColor(.primary)
+                }
+                Button(action: bellButtonAction) {
+                    Image(systemName: "bell")
+                        .font(.system(size: 24))
+                        .foregroundColor(.primary)
+                }
+            }
+        }
     }
 }
 
