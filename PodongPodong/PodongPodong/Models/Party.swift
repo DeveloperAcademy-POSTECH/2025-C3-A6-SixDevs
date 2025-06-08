cffb331da4d0866f29593359eaf8efa6f92914de
Paidion <dev.paidion@gmail.com>
2025-06-08 21:42:13 +0900

    feat: 검색 목록 UI 추가 (초기 화면, 로딩중, 목록 없는 화면)
    - Models/Party 는 Kinder 파일과 동일하게 맞춤

--- PodongPodong/PodongPodong/Models/Party.swift
+++ PodongPodong/PodongPodong/Models/Party.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 struct Party: Codable, Identifiable {
-    
     // 파티 생성시 만들어져야할 데이터
     /// id(UUID), 작성자, 파티원,  생성일, 업데이트일, 참여 대기 인원
     var id: UUID = UUID() // .toString
@@ -18,23 +17,25 @@ struct Party: Codable, Identifiable {
     private(set) var updatedAt: Date = Date()
     private(set) var status: PartyStatus = .recruiting // 파티 상태
     private(set) var waitingMembers: [User] = [] // 파티 참여 대기 인원
+    private(set) var viewCount: Int = 1
+    private(set) var likeCount: Int = 0
     
     // 유저가 입력해야할 데이터(필수)
     /// 제목, 카테고리, 구매방식, 모집인원, 구매처, 총 가격, 무게 및 수량
-    let title: String
-    let category: FoodCategory // 카테고리 : 육류, 채소, 과일, 해산물, 기타
+    var title: String
+    var category: FoodCategory // 카테고리 : 육류, 채소, 과일, 해산물, 기타
     let orderType: OrderType // 구매 방식 : 공동구매, 장보기
-    let recruitmentCount: Int // 모집 인원 수 : 2 ~ 8
-    let purchaseChannel: PurchaseChannel // 구매처 (온라인 / 오프라인)
-    let purchaseLocation: String // 입력받은 구매 장소 (URL || 장소)
-    let totalPrice: Int
-    let weightAndCount: WeightAndCount
-    let amount: Int
+    var recruitmentCount: Int // 모집 인원 수 : 2 ~ 8
+    var purchaseChannel: PurchaseChannel // 구매처 (온라인 / 오프라인)
+    var purchaseLocation: String // 입력받은 구매 장소 (URL || 장소)
+    var totalPrice: Int
+    var weightAndCount: WeightAndCount
+    var amount: Int
     
     // 유저가 입력해야할 데이터(필수X)
     // 약속 일정, 상세 내용
-    let appointment: Appointment
-    let description: String?
+    var appointment: Appointment
+    var description: String?
     
     // 부가적인 데이터
     var comments: [PartyComment] = [] // 댓글
--- PodongPodong/PodongPodong/Presentations/Search/Views/SearchView.swift
+++ PodongPodong/PodongPodong/Presentations/Search/Views/SearchView.swift
@@ -11,86 +11,75 @@ struct SearchView: View {
     @StateObject private var viewModel = SearchViewModel()
     @State private var filterContentHeight: CGFloat = 250
     @FocusState private var isTextFieldFocused: Bool
-    
+
     var body: some View {
         ZStack {
+            Color.clear.ignoresSafeArea()
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    isTextFieldFocused = false
+                }
+
             VStack(spacing: 0) {
                 // 1. 상단 검색바 (내비게이션, 검색 텍스트필드)
                 SearchHeaderView(text: $viewModel.searchText)
                     .focused($isTextFieldFocused)
-                
+
                 // 2. 공동 구매 / 장보기 (탭버튼)
                 SearchTabHeader(
-                    selectedTab: $viewModel.selectedTab,
+                    selectedTab: $viewModel.selectedOrderType,
                     purchaseChannel: $viewModel.purchaseChannel
                 )
                 .frame(height: 47)
                 .overlay(Divider(), alignment: .bottom)
-                
+
                 // 3. (모집중-진행중만 보기 / 필터 버튼)
                 SearchList_FilterHeader(
-                    isShowingNotCompletedPartyOnly: $viewModel.isShowingNotCompletedPartyOnly,
-                    isFilterApplied: viewModel.isFilterApplied) {
-                        viewModel.isShowingNotCompletedPartyOnly ?
-                        (viewModel.isShowingNotCompletedPartyOnly = false) :
-                        (viewModel.isShowingNotCompletedPartyOnly = true)
-                    } filterButtonAction: {
-                        viewModel.isShowingFilterView ?
-                        (viewModel.isShowingFilterView = false) :
-                        (viewModel.isShowingFilterView = true)
-                    }
-                
-                // 4. 검색 목록
-                TabView(selection: $viewModel.selectedTab) {
-                    VStack {
-                        Spacer()
-                        
-                        VStack(spacing: 10) {
-                            Text("공동구매 콘텐츠")
-                                .font(.pretendardMedium18)
-                            Text("검색어: \(viewModel.searchText)")
-                            Text("모집중 - 진행중인 파티: \(viewModel.isShowingNotCompletedPartyOnly)")
-                            Text("카테고리: \(viewModel.foodCategory)")
-                            Text("구매처: \(viewModel.purchaseChannel)")
-                            Text("필터링뷰: \(viewModel.isShowingFilterView)")
-                        }
-                        
-                        Spacer()
-                    }
-                    .tag(PartyListTab.공동구매)
-                    
-                    VStack {
-                        Spacer()
-                        Text("장보기 콘텐츠")
-                            .font(.pretendardMedium18)
-                        Text("검색어: \(viewModel.searchText)")
-                        Text("모집중 - 진행중인 파티: \(viewModel.isShowingNotCompletedPartyOnly)")
-                        Text("카테고리: \(viewModel.foodCategory)")
-                        Text("구매처: \(viewModel.purchaseChannel)")
-                        Text("필터링뷰: \(viewModel.isShowingFilterView)")
-                        Spacer()
-                    }
-                    .tag(PartyListTab.장보기)
+                    isShowingNotCompletedPartyOnly: $viewModel
+                        .isShowingNotCompletedPartyOnly,
+                    isFilterApplied: viewModel.isFilterApplied
+                ) {
+                    viewModel.isShowingNotCompletedPartyOnly
+                        ? (viewModel.isShowingNotCompletedPartyOnly = false)
+                        : (viewModel.isShowingNotCompletedPartyOnly = true)
+                } filterButtonAction: {
+                    viewModel.isShowingFilterView
+                        ? (viewModel.isShowingFilterView = false)
+                        : (viewModel.isShowingFilterView = true)
                 }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+
+                // 4. 검색 목록
+                if viewModel.searchText.isEmpty {
+                    Spacer()
+                    VStack(alignment: .center, spacing: 16) {
+                        Text("검색어를 입력하여")
+                        Text("원하는 파티를 검색해보세요")
+                    }
+                    .font(.pretendardMedium16)
+                    .foregroundStyle(Color.gray80)
+                    Spacer()
+                } else {
+                    SearchMainView(
+                        selectedTab: $viewModel.selectedOrderType,
+                        isLoading: $viewModel.isLoading,
+                        parties: viewModel.partyList
+                    )
+                }
             }
             
             // 5. 필터 바텀 시트
             .sheet(isPresented: $viewModel.isShowingFilterView) {
-                SearchFilterView(viewModel: viewModel, initialFoodCategory: viewModel.foodCategory,
-                                 initialPurchaseChannel: viewModel.purchaseChannel,                            contentHeight: $filterContentHeight)
+                SearchFilterView(
+                    viewModel: viewModel,
+                    contentHeight: $filterContentHeight
+                )
                 .presentationDetents([.height(filterContentHeight)])
                 .presentationDragIndicator(.visible)
             }
         }
-        .onTapGesture {
-            isTextFieldFocused = false
-        }
     }
-        
+
 }
-
-
 
 #Preview {
     NavigationStack {
