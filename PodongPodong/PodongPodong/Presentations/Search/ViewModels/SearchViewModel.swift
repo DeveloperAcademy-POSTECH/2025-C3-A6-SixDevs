1913a9336c328d00c0a200fad64b0197a1124d99
Paidion <dev.paidion@gmail.com>
2025-06-08 21:38:42 +0900

    fix: 검색어 없으면 목록 불러오지 않도록 수정

--- PodongPodong/PodongPodong/Presentations/Search/ViewModels/SearchViewModel.swift
+++ PodongPodong/PodongPodong/Presentations/Search/ViewModels/SearchViewModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import Combine
 
 final class SearchViewModel: ObservableObject {
-    var selectedTab: PartyListTab = .공동구매
-
+    var selectedOrderType: OrderType = .groupPurchase
+    
     @Published var searchText: String = ""
     @Published var isLoading: Bool = false
     @Published var isShowingFilterView: Bool = false
@@ -28,34 +28,34 @@ final class SearchViewModel: ObservableObject {
     @Published private(set) var partyList: [Party] = []
 
     private var cancellables = Set<AnyCancellable>()
-
+    
     init() {
         Publishers.CombineLatest4($isShowingNotCompletedPartyOnly, $foodCategory, $purchaseChannel, $searchText)
             .debounce(for: .milliseconds(300), scheduler: RunLoop.main)
-            .sink { [weak self] _, _, _, _ in
+            .sink { [weak self] _, _, _, text in
+                //MARK: 검색어가 없으면 fetch 하지 않음
+                guard !text.isEmpty else { return }
                 Task { await self?.fetchPartyList() }
             }
             .store(in: &cancellables)
     }
-
+    
     func fetchPartyList() async {
-        Task {
-            do {
-                let parties: [Party] = try await FirestoreManager.shared.fetch(
-                    as: Party.self,
-                    .party,
-                    whereFeild: "title",
-                    // equalData: searchText.isEmpty ? nil : searchText,
-                    // count: 0
-                )
-                await MainActor.run {
-                    self.partyList = parties
-                }
-            } catch {
-                print("파티 목록을 불러오는데 실패했습니다: \(error.localizedDescription)")
+        await MainActor.run { self.isLoading = true }
+        defer { Task { @MainActor in self.isLoading = false } }
+        do {
+            let parties: [Party] = try await FirestoreManager.shared.fetch(
+                as: Party.self,
+                .party,
+                whereFeild: "title",
+                equalData: searchText.isEmpty ? nil : searchText,
+                count: 0
+            )
+            await MainActor.run {
+                self.partyList = parties
             }
+        } catch {
+            print("파티 목록을 불러오는데 실패했습니다: \(error.localizedDescription)")
         }
     }
-    
-    
 }
