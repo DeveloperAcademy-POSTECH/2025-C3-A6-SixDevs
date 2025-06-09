3011c8a1bbf8e1005e0c0df3faa3b4b6d819b88e
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-09 19:23:44 +0900

    refactor: 무게와 개수를 받는 부분 바인딩 값 수정

--- PodongPodong/PodongPodong/Presentations/PartyCreate/ViewModels/PartyCreateViewModel.swift
+++ PodongPodong/PodongPodong/Presentations/PartyCreate/ViewModels/PartyCreateViewModel.swift
@@ -13,15 +13,26 @@ final class PartyCreateViewModel {
     var selectedOrderType: OrderType? = nil
     var title: String = ""
     var selectedCategory: FoodCategory? = nil
-    var recruitmentCount = 2 //참여 인원
+    var recruitmentCount = 2
     var selectedPurchaseChannle: PurchaseChannel? = nil
-    
+    var purchaseLocation: String = ""
+
     var totalPrice: Int? = nil
-    var selectedweightAndCount: WeightAndCount? = nil
+    var selectedWeightAndCount: WeightAndCount? = nil
     var amount: Int? = nil
-    
+
     var selectedDate: String = ""
     var selectedTime: String = ""
     var selectedPlace: String = ""
     var description: String = ""
+
+    var isButtonEnabled: Bool {
+        selectedOrderType != nil &&
+        !title.isEmpty &&
+        selectedCategory != nil &&
+        selectedPurchaseChannle != nil &&
+        totalPrice != nil &&
+        selectedWeightAndCount != nil &&
+        amount != nil
+    }
 }
