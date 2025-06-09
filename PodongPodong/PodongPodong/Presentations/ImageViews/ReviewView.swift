7a86c9a9c8de9f04595c076d57d58a55eea7f83a
Seo <seohyun6361@gmail.com>
2025-06-10 01:14:56 +0900

    feat: 이미지로 처리하는 View 추가

---
+++ PodongPodong/PodongPodong/Presentations/ImageViews/ReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewView.swift
+//  PodongPodong
+//
+//  Created by 김서현 on 6/9/25.
+//
+// MARK: - 마이페이지> 내가 받은 후기 View
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Image("ReviewViewImage")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ReviewView()
+}
