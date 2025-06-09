6c80680aa5932e820d9a2a2df862cbca0b3fd90c
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-09 16:23:22 +0900

    refactor: 설명 부분 뷰 분할

--- PodongPodong/PodongPodong/Presentations/PartyCreate/Views/PartyCreateView.swift
+++ PodongPodong/PodongPodong/Presentations/PartyCreate/Views/PartyCreateView.swift
@@ -21,6 +21,7 @@ struct PartyCreateView: View {
     @State var selectedDate: String = ""
     @State var selectedTime: String = ""
     @State var selectedPlace: String = ""
+    @State var description: String = ""
     
     var body: some View {
         ScrollView{
@@ -42,38 +43,12 @@ struct PartyCreateView: View {
                     selectedTime: $selectedTime,
                     selectedPlace: $selectedPlace
                 )
-                descriptionView
+                DescriptionView(description: $description)
             }
         }
     }
-        
-    // MARK: - descriptionView 설명(선택)
-    @ViewBuilder
-    var descriptionView: some View {
-        VStack{
-            Text("설명 (선택)")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 16)
-                .font(.pretendardMedium16)
-            Text("이런 내용을 적어보세요\n-가이드라인 1\n-가이드라인2\n-가이드라인3")
-                .padding(.horizontal, 20)
-                .padding(.vertical, 12)
-                .padding(.trailing, 152)
-                .frame(width: 361)
-                .background(Color.gray10)
-                .foregroundColor(Color.gray70)
-                .font(.pretendardMedium16)
-                
-                
-            // TODO: 멀티라인 입력 처리하기
-            TextFieldView(
-                text: .constant(""),
-                placeholder: "파티를 어떻게 운영할 것인지에 대한 구체적인 정보를 적어주세요.").frame(width: 361, height: 244)
-        } //: VSTACK
-    }
 }
 
-// TODO: 필수항목 선택항목 로직 추가해야함 (필수 항목 입력 안 하면 안 넘어가게)
 #Preview {
     PartyCreateView()
 }
