3a8980141e48a07da1e05b06798b10ef6a69398f
Velko <wlsgurrla716@naver.com>
2025-06-11 15:23:39 +0900

    feat/#66 고양이 이미지 Assets 추가

---
+++ PodongPodong/PodongPodong/Presentations/MyPage/Components/ProfileItem.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileItem.swift
+//  PodongPodong
+//
+//  Created by 김진혁 on 6/11/25.
+//
+
+import SwiftUI
+
+struct ProfileItem: View {
+    let image: String
+    
+    var body: some View {
+        Circle()
+    }
+}
+
+#Preview {
+    ProfileItem(image: "")
+}
