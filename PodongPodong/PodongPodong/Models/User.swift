184901db0b7c55e7da8b0d41bcf5c9940639c686
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-11 22:30:54 +0900

    fix: 유저의 프로필 이미지를 String 데이터로 변경

--- PodongPodong/PodongPodong/Models/User.swift
+++ PodongPodong/PodongPodong/Models/User.swift
@@ -10,29 +10,8 @@ struct User: Identifiable, Codable {
     let id: UUID = UUID()
     let email: String
     let nickName: String // 앱 활동 이름
-    var profileImage: ProfileImage    // Asset 에서 불러오기
+    var profileImage: String    // Asset 에서 불러오기
     var reviewRating: Double // 리뷰 평점
-
-    // TODO: Asset 이름과 동일하게 수정하기
-    enum ProfileImage: Codable {
-        case image1
-        case image2
-        case image3
-        case image4
-        
-        var profileImage: String {
-            switch self {
-            case .image1:
-                return "1번 이미지" // FIXME: -
-            case .image2:
-                return "2번 이미지" // FIXME: -
-            case .image3:
-                return "3번 이미지" // FIXME: -
-            case .image4:
-                return "4번 이미지" // FIXME: -
-            }
-        }
-    }
 }
 
 extension User: EntityRepresentable {
