3a08c4a5873608f261f083cc4073189c6f5b67f1
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-06-11 12:23:53 +0900

    feat: 내 파티 UI 및 기능 구현

--- PodongPodong/PodongPodong/Presentations/MyParties/ViewModels/MyPartiesViewModel.swift
+++ PodongPodong/PodongPodong/Presentations/MyParties/ViewModels/MyPartiesViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+final class MyPartiesViewModel {
+    var selectedPartyStatus: PartyStatus = .recruiting
+
+    private(set) var partyList: [Party] = []
+    private var user: User = User.sampleHost
+
+    init() {
+        partyList = DummyData.allParties
+    }
+
+    var myParties: [Party] {
+        partyList
+            .filter { $0.member.contains { $0.id == user.id } }
+            .filter { $0.status == selectedPartyStatus }
+    }
+}
--- PodongPodong/PodongPodong/Presentations/MyParties/Views/MyPartiesView.swift
+++ PodongPodong/PodongPodong/Presentations/MyParties/Views/MyPartiesView.swift
@@ -8,71 +8,32 @@
 import SwiftUI
 
 struct MyPartiesView: View {
-//    @State private var viewModel = MyPartiesViewModel()
-//    @Binding var selectedTab: PartyStatus
-    
-    var body: some View {
-        VStack(spacing: 0) {
-            Text("내가 참여한 파티")
-                .font(.pretend(type: .bold, size: 18))
-            
-//            GeometryReader { geo in
-//                let totalWidth = geo.size.width
-//                let tabCount = CGFloat(OrderType.allCases.count)
-//                let indicatorWidth = totalWidth / tabCount
-//                
-//                VStack(spacing: 0) {
-//                    HStack(spacing: 0) {
-//                        ForEach(OrderType.allCases) { tab in
-//                            Button {
-//                                withAnimation(.easeInOut) {
-//                                    selectedTab = tab
-//                                }
-//                            } label: {
-//                                Text(tab.title)
-//                                    .font(
-//                                        selectedTab == tab
-//                                        ? .pretendardBold18
-//                                        : .pretendardMedium18
-//                                    )
-//                                    .foregroundColor(
-//                                        selectedTab == tab
-//                                        ? Color.secondary
-//                                        : Color.gray40
-//                                    )
-//                                    .frame(maxWidth: .infinity)
-//                                    .frame(height: 44)
-//                            }
-//                        }
-//                    }
-//                    .frame(width: totalWidth)
-//                    .background(Color.white)
-//                    
-//                    Rectangle()
-//                        .fill(Color.secondary)
-//                        .frame(width: indicatorWidth, height: 3)
-//                        .frame(maxWidth: .infinity, alignment: .leading)
-//                        .offset(x: indicatorWidth * CGFloat(selectedTab.rawValue))
-//                        .animation(.easeInOut, value: selectedTab)
-//                }
-//                .frame(width: totalWidth, alignment: .leading)
-//            }
-//            .frame(maxWidth: .infinity, alignment: .leading)
-//            .frame(height: 47)
-//            .overlay(Divider(), alignment: .bottom)
+    @State private var viewModel = MyPartiesViewModel()
 
-            
-            Image("내참파")
-                .resizable()
- 
+    var body: some View {
+        NavigationView {
+            ZStack(alignment: .top) {
+                Color.clear.ignoresSafeArea()
+
+                VStack {
+                    Text("내가 참여한 파티")
+                        .font(.pretend(type: .bold, size: 18))
+
+                    MyPartiesTabHeader(
+                        selectedTab: $viewModel.selectedPartyStatus
+                    )
+                    .frame(height: 47)
+                    .overlay(Divider(), alignment: .bottom)
+
+                    MyPartiesMainView(
+                        selectedTab: $viewModel.selectedPartyStatus,
+                        myParties: viewModel.myParties
+                    )
+                }
+            }
         }
     }
-    
-    
-    
-    
 }
-
 
 #Preview {
     MyPartiesView()
